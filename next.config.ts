7e2b4a20c93b1c5017410628eb942b681b3f7b99
Jiayi Guo <studentjiayi@gmail.com>
2025-02-27 17:20:00 +0100

    adjust config again

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
-
-export default nextConfig;
-
-module.exports = {
-  experimental: {
-    // Disable the Suspense error if you're not using Suspense boundaries
-    reactMode: 'concurrent',
-  },
-};
-
-module.exports = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-}
+};
