fb4b774a4d7bb3cfac0909f16e2f2db4b220c673
HelgeKv <helge.kvarfordt@gmail.com>
2025-03-09 18:08:18 +0100

    Small radarchart change

--- components/radartest.tsx
+++ components/radartest.tsx
@@ -108,11 +108,7 @@ export default function Radartest({ songsData }: { songsData: any[] }) {
 
   return (
     <div className="text-center">
-      {data.length === 0 ? (
-        <p>Choose songs in Song List</p>
-
-
-      ) : (
+        {data.length === 0 ? (<p>Choose songs in Song List</p> ) : ("")}
         <div className="w-[430px] h-[430px]"> {/* Enlarged Chart */}
           <Radar
             data={chartData}
@@ -156,7 +152,7 @@ export default function Radartest({ songsData }: { songsData: any[] }) {
             }}
           />
         </div>
-      )}
+
     </div>
   );
 }
