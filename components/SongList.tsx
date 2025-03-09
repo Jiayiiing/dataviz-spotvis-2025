636c47bc226e8b733f8debeb91cf999df4c9df42
Mille Kåge <89786843+Wolframe107@users.noreply.github.com>
2025-03-09 20:02:51 +0100

    Added link to song

--- components/SongList.tsx
+++ components/SongList.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { getAlbumCover } from "@/utils/SpotifyAPI/spotifyApi";
-import { useState, useEffect} from "react";
-
+import { useState, useEffect } from "react";
 
 type Song = {
   name: string;
@@ -12,8 +11,9 @@ type Song = {
   valence: number;
   acousticness: number;
   popularity: number;
-  liveness: number; 
-  Song_artists: SongArtist[];};
+  liveness: number;
+  Song_artists: SongArtist[];
+};
 
 type Artist = { name: string };
 type SongArtist = { Artists: Artist };
@@ -39,12 +39,14 @@ export default function SongList({
   selectedArtists,
   onSelectionChange,
 }: SongListProps) {
-
-  const [albumCovers, setAlbumCovers] = useState<Record<string, string | null>>({});
-  const uniqueDates = Array.from(new Set(rankings.map((ranking) => ranking.snapshot_date))).sort();
+  const [albumCovers, setAlbumCovers] = useState<Record<string, string | null>>(
+    {}
+  );
+  const uniqueDates = Array.from(
+    new Set(rankings.map((ranking) => ranking.snapshot_date))
+  ).sort();
   const [selectedDate, setSelectedDate] = useState<string>(""); // Start as empty
   const [hasUserInteracted, setHasUserInteracted] = useState<boolean>(false); // Tracks slider interaction
-
 
   // Ensure selectedDate is set to uniqueDates[0] in the beginning.
   useEffect(() => {
@@ -53,12 +55,13 @@ export default function SongList({
     }
   }, [uniqueDates, selectedDate]); // Runs when uniqueDates or selectedDate changes
 
-  
   useEffect(() => {
     const fetchAlbumCovers = async () => {
-      const uniqueIds = Array.from(new Set(rankings.map((entry) => entry.spotify_id))); // Unique IDs
+      const uniqueIds = Array.from(
+        new Set(rankings.map((entry) => entry.spotify_id))
+      ); // Unique IDs
       const covers: Record<string, string | null> = {};
-  
+
       await Promise.all(
         uniqueIds
           .filter((id) => !albumCovers[id]) // Only fetch if not already in state
@@ -67,18 +70,19 @@ export default function SongList({
             covers[spotifyId] = cover;
           })
       );
-  
+
       setAlbumCovers((prevCovers) => ({ ...prevCovers, ...covers }));
     };
-  
+
     fetchAlbumCovers();
   }, [rankings]);
-  
-  
+
   const handlePlusButtonClick = (song: Song) => {
     if (selectedSongs.some((s) => s.spotify_id === song.spotify_id)) {
       // Deselect song
-      onSelectionChange(selectedSongs.filter((s) => s.spotify_id !== song.spotify_id));
+      onSelectionChange(
+        selectedSongs.filter((s) => s.spotify_id !== song.spotify_id)
+      );
     } else {
       onSelectionChange([...selectedSongs, song]);
     }
@@ -87,7 +91,7 @@ export default function SongList({
   const handleSliderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedIndex = parseInt(e.target.value, 10);
     const actualDate = uniqueDates[selectedIndex]; // Get the actual date using the index
-    setSelectedDate(actualDate); 
+    setSelectedDate(actualDate);
     setHasUserInteracted(true);
     console.log(actualDate);
   };
@@ -96,7 +100,7 @@ export default function SongList({
 
   return (
     <div className="relative">
-       {/* Clear Selection Button */}
+      {/* Clear Selection Button */}
       <button
         onClick={() => onSelectionChange([])}
         className="absolute top-0 right-0 mt-2 mr-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 shadow-md"
@@ -108,7 +112,9 @@ export default function SongList({
       {/* Date Selector */}
       <div className="mb-4">
         <label className="mr-2">Selected Date:</label>
-        <span className="text-lg font-semibold text-blue-500">{selectedDate}</span>
+        <span className="text-lg font-semibold text-blue-500">
+          {selectedDate}
+        </span>
         <input
           type="range"
           min={0}
@@ -116,14 +122,13 @@ export default function SongList({
           value={uniqueDates.indexOf(selectedDate)}
           onChange={handleSliderChange}
           className="w-full"
-          onClick={(e) => e.stopPropagation()} 
+          onClick={(e) => e.stopPropagation()}
         />
-         <div className="flex justify-between">
+        <div className="flex justify-between">
           <span>{uniqueDates[0]}</span>
           <span>{uniqueDates[uniqueDates.length - 1]}</span>
         </div>
       </div>
-
 
       {/* Song Cards */}
       <div className="space-y-4">
@@ -141,11 +146,17 @@ export default function SongList({
               // Filter songs based on selected artists
               if (selectedArtists.length === 0) return true; // If no artists are selected, show all songs
               return ranking.Songs.Song_artists.some((songArtist) =>
-                selectedArtists.some((artist) => artist.name.trim() === songArtist.Artists.name.trim())
+                selectedArtists.some(
+                  (artist) =>
+                    artist.name.trim() === songArtist.Artists.name.trim()
+                )
               );
             })
             .map((ranking) => (
-              <div key={ranking.spotify_id} className="flex items-center p-4 h-20 border border-gray-300 rounded-lg shadow-md hover:shadow-lg">
+              <div
+                key={ranking.spotify_id}
+                className="flex items-center p-4 h-20 border border-gray-300 rounded-lg shadow-md hover:shadow-lg"
+              >
                 <div className="w-16 h-16 mr-4">
                   {albumCovers[ranking.spotify_id] ? (
                     <img
@@ -159,26 +170,37 @@ export default function SongList({
                 </div>
                 <div className="flex-1">
                   <div className="text-sm text-white">
-                    <span className="font-bold">Title:</span> {ranking.Songs.name}
+                    <span className="font-bold">Title:</span>{" "}
+                    <a
+                      href={`https://open.spotify.com/track/${ranking.Songs.spotify_id}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ color: "rgb(29,185,84)" }}
+                      className="underline"
+                    >
+                      {ranking.Songs.name}
+                    </a>
                   </div>
                   <div className="text-sm text-white">
-                    <span className="font-bold">Artists:</span> {ranking.Songs.Song_artists
-                      .map((songArtist) => songArtist.Artists.name)
-                      .join(", ") || "Unknown Artist"}
+                    <span className="font-bold">Artists:</span>{" "}
+                    {ranking.Songs.Song_artists.map(
+                      (songArtist) => songArtist.Artists.name
+                    ).join(", ") || "Unknown Artist"}
                   </div>
                   <div className="text-sm text-white">
-                    <span className="font-bold">Rank:</span> {ranking.daily_rank}
+                    <span className="font-bold">Rank:</span>{" "}
+                    {ranking.daily_rank}
                   </div>
                 </div>
                 <button
                   onClick={(event) => {
-                    event.stopPropagation(); 
-                    handlePlusButtonClick(ranking.Songs);}}
+                    event.stopPropagation();
+                    handlePlusButtonClick(ranking.Songs);
+                  }}
                   className={`p-2 ml-auto rounded ${selectedSongs.some((s) => s.spotify_id === ranking.Songs.spotify_id) ? "bg-green-500 text-white" : "bg-gray-300 text-black"}`}
                 >
                   +
                 </button>
-                
               </div>
             ));
         })()}
